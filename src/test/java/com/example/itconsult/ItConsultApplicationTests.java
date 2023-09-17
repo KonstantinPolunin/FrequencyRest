ac510c56ba35050d1a58f39019652a659de3bb1c
Polunin Konstantin <tinyyy052@gmail.com>
2023-09-17 23:40:37 +0300

    add service

--- src/test/java/com/example/itconsult/ItConsultApplicationTests.java
+++ src/test/java/com/example/itconsult/ItConsultApplicationTests.java
@@ -1,10 +1,16 @@
 package com.example.itconsult;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@WebMvcTest
 class ItConsultApplicationTests {
+    
+    private MockMvc mockMvc;
 
     @Test
     void contextLoads() {
